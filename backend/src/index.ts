144294b714a16cb722c9614626172bc10156c6c3
gauravmehraa <gauravmehra158@gmail.com>
2024-08-19 23:10:14 +0530

    [refactor]: removed unused controllers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,6 @@ import studentRoutes from "./routes/student.routes";
 import notesRoutes from "./routes/notes.routes";
 import subjectRoutes from "./routes/subject.routes";
 import quizRoutes from "./routes/quiz.routes";
-import questionRoutes from "./routes/question.routes";
 import gradeRoutes from "./routes/grades.routes";
 import feedbackRoutes from "./routes/feedback.routes";
 
@@ -28,7 +27,6 @@ app.use('/api/student/', studentRoutes);
 app.use('/api/subject', subjectRoutes);
 app.use('/api/notes', notesRoutes);
 app.use('/api/quiz', quizRoutes);
-app.use('/api/question', questionRoutes);
 app.use('/api/feedback', feedbackRoutes);
 app.use('/api/grades', gradeRoutes);
 
