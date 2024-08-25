2fd82121bd1ad6228ac8410d2ff2b3bac832e414
gauravmehraa <gauravmehra158@gmail.com>
2024-08-26 02:01:16 +0530

    [feat]: added feedback pagination

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   const { authUser } = useAuthContext();
   const { loading, logout } = useLogout();
 
-  const buttonClass = "w-11/12 justify-left items-center flex gap-4 px-6 py-3 rounded-md"
+  const buttonClass = "w-11/12 justify-left items-center flex gap-4 px-6 py-3 rounded-md focus:outline-none"
 
   const handleClose: any = async(e: MouseEvent) => {
     await sleep(200);
@@ -27,10 +27,10 @@ const Navbar = () => {
     <nav className='max-h-screen min-h-screen border-r-0 border-slate-400 min-w-72 max-w-72 bg-white md:flex flex-col text-black font-roboto hidden'> 
       <img 
         src={'https://www.lms.org/files/assets/logo.png'}
-        className='w-6/12 mx-auto my-6'
+        className='w-6/12 mx-auto my-4'
         alt='ClassHub Logo'
       />
-      <div className='mt-auto flex flex-col items-center gap-4 py-10'>
+      <div className='mt-auto flex flex-col items-center gap-4 pb-6'>
         <NavLink to="dashboard" className={({ isActive }) => isActive ? `${buttonClass} bg-classhub-purple text-white glass` : buttonClass}>
           <GoGraph className='w-6 h-6'/> Dashboard
         </NavLink>
@@ -52,7 +52,7 @@ const Navbar = () => {
           <MdHelpOutline className='w-6 h-6'/> Help
         </NavLink>
       </div>
-      <div className='mt-auto flex flex-col items-center gap-6 pb-16 font-semibold'>
+      <div className='mt-auto flex flex-col items-center gap-6 my-auto font-semibold'>
         <NavLink to="help" className='flex flex-row items-center gap-3'>
           <TbSettings className='w-6 h-6'/> Settings
         </NavLink>
--- frontend/src/pages/Feedback.tsx
+++ frontend/src/pages/Feedback.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import FeedbackCard from "../components/FeedbackCard";
 import useGetFeedback from "../hooks/feedback/useGetFeedback";
 
 const Feedback = () => {
   const { loading, feedbacks } = useGetFeedback();
+
+  const [page, setPage] = useState(1);
+
+  const itemsPerPage = 2;
+  const pages = Math.ceil(feedbacks.length / itemsPerPage);
+  const values = Array.from({ length: pages }, (_, i) => i + 1);
+
+  const start = (page - 1) * itemsPerPage;
+  const end = page * itemsPerPage;
+  const currentPage = feedbacks.slice(start, end);
+
   return (
     <div className="flex flex-col text-black p-4 sm:p-8 overflow-auto max-h-screen w-full">
       <div className="text-3xl text-center font-semibold py-4">Feedbacks</div>
@@ -13,13 +25,35 @@ const Feedback = () => {
       <div className="flex flex-col gap-4 m-2 sm:m-8 items-center">
         { feedbacks.length === 0?
           <div className="mx-auto"> No feedback to show </div>:
-          feedbacks.map((feedback: { _id: React.Key | null | undefined; }, index: number) => (
+          <>
+            <div className="join shadow-md">
+              {values.map((value) => (
+                //
+                <label key={value} htmlFor={`radio-${value}`} className={`join-item btn btn-square border-none cursor-pointer ${page === value ? 'bg-classhub-purple text-black' : 'bg-white text-black'}`}>
+                  <input
+                    id={`radio-${value}`}
+                    className="hidden" 
+                    value={value}
+                    onChange={() => setPage(value)}
+                    type="radio"
+                    name="options"
+                    aria-label={value.toString()}
+                    checked={page === value}
+                  />
+                  {value}
+              </label>
+              ))}
+            </div>
+
+           {currentPage.map((feedback: { _id: React.Key | null | undefined; }, index: number) => (
             <FeedbackCard
               key={feedback._id}
               data={feedback}
               showInfo={true}
             />
-          ))}
+          ))  }
+          </>
+        }
       </div>
     }
   </div>
